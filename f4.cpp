a4d1f396e369a7667434e9b88ac7bd9821b9f6cb
gevorik3991 <92302057+gevorik3991@users.noreply.github.com>
2021-10-15 10:16:54 +0300

    Update f4.cpp

--- f4.cpp
+++ f4.cpp
@@ -38,14 +38,36 @@ int itc_covert_num(long long number, int ss){
 }
 
 
-int itc_rev_covert_num(long long number, int ss){
-    int a = 0, b = 0;
-    if(ss == 10)
-        return number;
-    while(number > 0){
-        a += number % 10 * itc_pow(ss, b);
-        number /= 10;
-        b++;
+long long itc_covert_num(long long number, int i)
+{
+    if(num < 0)
+        return -1;
+    if(i > 10 || i < 2)
+        return -1;
+    long long num2 = num;
+    long long bin1 = 0;
+    long long bin2 = 0;
+    bool isFirstOne = 0;
+    long long colOfNols = 0;
+    while (num2 > 0)
+    {
+        if(num2 % i == 0 && isFirstOne == 0){
+            colOfNols ++;
+        }
+        if(num % i != 0){
+            isFirstOne = 1;
+        }
+        bin1 = bin1 * 10 + (num2 % i);
+        num2 = num2 / i;
     }
-    return a;
+    //if(itc_max_num(bin1) > (i - 1))
+    //    return -1;
+    if(colOfNols > 0)
+        bin2 = my_rev(bin1) * my_ten_in_step(colOfNols);
+    else
+        bin2 = my_rev(bin1);
+    if(itc_rev_covert_num(bin2, i) == num){
+        return bin2;
+    }
+    return -1;
 }
